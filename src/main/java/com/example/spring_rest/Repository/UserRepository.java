ad1a28c67d7b1fcb351eeee3d0219c1955e2159b
master1346 <shedrin.yakudza46@gmail.com>
2021-12-24 14:41:46 +0300

    Commit 1

--- src/main/java/com/example/spring_rest/Repository/UserRepository.java
+++ src/main/java/com/example/spring_rest/Repository/UserRepository.java
@@ -2,25 +2,27 @@ package com.example.spring_rest.Repository;
 
 import com.example.spring_rest.Authorities;
 import com.example.spring_rest.Person;
-import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Repository;
-import org.springframework.stereotype.Service;
 
+
+import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 
 @Repository
 public class UserRepository {
-    private final List<Person> personList = Arrays.asList(new Person("jon","qwerty1", Arrays.asList(Authorities.DELETE, Authorities.WRITE)),
+    private final List<Person> personList = Arrays.asList(
+            new Person("jon","qwerty1", Arrays.asList(Authorities.DELETE, Authorities.WRITE)),
             new Person("Ben","abcd1", Collections.singletonList(Authorities.WRITE)),
             new Person("Mike","123E", Arrays.asList(Authorities.DELETE, Authorities.WRITE, Authorities.READ))
     );
     public List<Authorities> getUserAuthorities(String user, String password) {
-        for(Person person : personList) {
-            if((person.getUser().equals(user)) && (person.getPassword().equals(password))){
-                return person.getAuthoritiesList();
-            }
+        if((user.equals("jon")) && (password.equals("qwerty1"))){
+             return new ArrayList<Authorities>(Collections.singleton(Authorities.READ));
+        }
+        if((user.equals("Ben")) && (password.equals("abcd"))){
+            return  new ArrayList<Authorities>(Collections.singleton(Authorities.WRITE));
         }
 
         return Collections.emptyList();
